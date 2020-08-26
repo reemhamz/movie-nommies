089b318c940b8108eb57e25a3ee8e255ccf100b0
Reem H <reem@fueled.com>
2020-08-25 23:58:12 -0400

    connected API🎥

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 import "./styles/App.scss";
+import axios from "axios";
+import { OMDBKey } from "./config";
 
 function App() {
-  return (
-    <div className="App">
+  const [movieList, setMovieList] = useState([]);
+  const [movieTitle, setMovieTitle] = useState("");
+
+  // API dependencies
+  const apiKey = OMDBKey;
+  const dataUrl = `http://www.omdbapi.com/?s=&apikey=${apiKey}&`;
+  const posterUrl = `http://img.omdbapi.com/?apikey=${apiKey}&`;
+  const movieSearch = "harry potter"
+
+
+  // HTTPS request to API (OMDB)
+  useEffect(() => {
+    axios({
+      method: "GET",
+      url: dataUrl,
+      params: {
+        // t: movieTitle,
+        s: movieSearch
+      }
+    }).then(res => {
+      // console.log(res.data.Search)
+      setMovieList(res.data.Search)
       
-    </div>
-  );
+    })
+  },[]);
+
+  return <div className="App">
+    {console.log(movieList)}
+  </div>;
 }
 
 export default App;
---
+++ src/config.js
@@ -0,0 +1,1 @@
+export const OMDBKey = "43090bb1";
