cd94c183840d766be2bf40b6a2624d16ff824ef3
Reem H <reem@fueled.com>
2020-08-27 21:40:52 -0400

    updates states

--- src/components/ApiCall.js
+++ src/components/ApiCall.js
@@ -1,16 +1,20 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { OMDBKey } from "../config.js";
+
+
 
 function App() {
   const [movieList, setMovieList] = useState([]);
   const [movieTitle, setMovieTitle] = useState("");
+  const [movieYear, setMovieYear] = useState("");
+  const [moviePoster, setMoviePoster] = useState("");
+
 
   // API dependencies
-  const apiKey = OMDBKey;
+  const apiKey = "43090bb1";
   const dataUrl = `http://www.omdbapi.com/?s=&apikey=${apiKey}&`;
-  const posterUrl = `http://img.omdbapi.com/?apikey=${apiKey}&`;
-  const movieSearch = "harry potter";
+  const posterUrl = `http://img.omdbapi.com/?s=apikey=${apiKey}&`;
+  const movieSearch = "twilight";
 
   // HTTPS request to API (OMDB)
   useEffect(() => {
@@ -18,21 +22,27 @@ function App() {
       method: "GET",
       url: dataUrl,
       params: {
-        // t: movieTitle,
         s: movieSearch,
       },
-    }).then((res) => {
-      setMovieList(res.data.Search);
-    });
+    })
+      .then((res) => {
+        res.data.Search.map((movieInfo) => {
+          if (movieInfo.Type === "movie") {
+            setMovieList(movieInfo);
+            setMovieTitle(movieInfo.Title);
+            setMovieYear(movieInfo.Year);
+            setMoviePoster(movieInfo.Poster);
+          }
+        });
+      })
+      .catch((err) => {
+        console.log("You've encountered a problem!");
+      });
   }, []);
 
   return (
     <div className="App">
-      <ul>
-        {movieList.map((data) => {
-          return <li>{data.Title}</li>;
-        })}
-      </ul>
+      
     </div>
   );
 }
--- src/config.js
+++
@@ -1,1 +0,0 @@
-export const OMDBKey = "43090bb1";
